5719656a10ab6f343934608913e0a98d0aa70e12
bonnieli1414 <bonnieli1414@gmail.com>
2022-08-15 10:33:23 +0800

    Update 2022-08-15T02:33:13.148Z

--- app/assets/js/all.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.ham-toggle').addEventListener("click", (e) => {
-    e.target.parentElement.classList.toggle('opened');
-    e.target.parentElement.nextElementSibling.classList.toggle('opened');
-});
